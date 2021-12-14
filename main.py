c120de1b6dbe771f19120e2439a9982e7c0898f5
broxy7 <philipp.steigerwald72@gmail.com>
2021-12-14 09:33:18 +0100

    added iteration through DF

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import matplotlib.pyplot as plt
 def use_filter(a, b, c):
 
     df = pd.read_csv("Results_2021-12-01_neuro_Cal-520_shorty-300s_mcor_Calima.csv")
+    df = df.iloc[:, 1:]
     frames = df.shape[0]
-    safer = np.zeros(shape=(frames, 1))
-    for i, row in enumerate(df.iloc[:, 1:31]):
-        for x, coloum in enumerate(df.iloc[:, i+1]):
+    means = df.shape[1]
+    safer = np.zeros(shape=(frames, means))
+    counter = 0
+    for j, mean in df.iteritems():
+        for x, row in enumerate(mean):
+            y = a * math.exp(-b * (x+1)) + c
+            safer[x, counter] = row - y
+        counter = counter + 1
 
-            y = a * math.exp(-b * x) + c
-            safer[x] = coloum - y
-            print(y)
-            print("he")
-    #     peaks, _ = find_peaks(df[data], prominence=12)
+        #     peaks, _ = find_peaks(df[data], prominence=12)
 
     plt.plot(safer)
     plt.show()
